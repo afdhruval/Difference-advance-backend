fa2c309718a6a5b66476196da21f53a485777bf9
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-03-24 22:52:32 +0530

    env detected

--- RAG/main.js
+++ RAG/main.js
@@ -2,9 +2,20 @@ import "dotenv/config"
 import { PDFParse } from "pdf-parse"
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { MistralAIEmbeddings } from "@langchain/mistralai";
+import { Pinecone } from '@pinecone-database/pinecone';
 import fs from "fs"
+import { text } from "stream/consumers";
+import { promises } from "dns";
 
 let databuffer = fs.readFileSync("./DHRUVAL_RESeME1.pdf")
+
+const pc = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY
+})
+
+const index = pc.Index("rag-pdf")
+
+
 
 const parser = new PDFParse({
     data: databuffer
@@ -24,6 +35,24 @@ const splitter = new RecursiveCharacterTextSplitter({
 
 const chunks = await splitter.splitText(data.text)
 
-const docs = await embeddings.embedDocuments(chunks)
+const docs = await Promise.all(chunks.map(async (chunk) => {
+    const embedding = await embeddings.embedQuery(chunk)
 
-console.log(docs);
+    return {
+        text: chunk,
+        embedding
+    }
+}))
+
+
+const results = await index.upsert({
+    records: docs.map((doc, i) => ({
+        id: `doc-${i}`,
+        values: doc.embedding,
+        metadata: {
+            text: doc.text
+        }
+    }))
+})
+
+console.log(results);
